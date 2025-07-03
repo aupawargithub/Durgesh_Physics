b70ed194865e1c3966c3022366cd540a2d2f7103
aupawargithub <107602489+aupawargithub@users.noreply.github.com>
2025-07-03 14:36:31 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,25 +1,24 @@
-
 let auth0 = null;
 
 const config = {
-  domain: "dev-7mt23fot8tx0n0ug.us.auth0.com",
-  client_id: "GHGUu4PxLHtzcFotqc5NhfCuq4GOyeBh",
-  redirect_uri: window.location.origin + "/Durgesh_Physics/home.html"
+domain: "dev-7mt23fot8tx0n0ug.us.auth0.com",
+client_id: "GHGUu4PxLHtzcFotqc5NhfCuq4GOyeBh",
+redirect_uri: window.location.origin + "/Durgesh_Physics/home.html"
 };
 
 const configureClient = async () => {
-  auth0 = await createAuth0Client({
-    domain: config.domain,
-    client_id: config.client_id,
-    redirect_uri: config.redirect_uri
-  });
+auth0 = await createAuth0Client({
+domain: config.domain,
+client_id: config.client_id,
+redirect_uri: config.redirect_uri
+});
 };
 
 const login = async () => {
-  await auth0.loginWithRedirect();
+await auth0.loginWithRedirect();
 };
 
 window.onload = async () => {
-  await configureClient();
-  document.getElementById("loginBtn").addEventListener("click", login);
+await configureClient();
+document.getElementById("loginBtn")?.addEventListener("click", login);
 };
